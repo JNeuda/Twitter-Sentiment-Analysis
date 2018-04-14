9f31f25f71f61faf36110460dd57b313ecc77bb9
mhager3 <mhager3@hotmail.com>
2018-04-14 13:13:02 -0400

    working copy, drop does not work

--- ApplyFilter.py
+++ ApplyFilter.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class ApplyFilter():
 
-    def ApplyFilter(df)
+    def ApplyFilter(df):
 
         # filter variables
         min_tweets = 5
@@ -11,12 +11,25 @@ class ApplyFilter():
         max_following = 2500
         lang = "en"
 
-        for row in df:
-            if (tweet["statuses"]["user"]["followers_count"] < max_followers and
-                tweet["statuses"]["user"]["statuses_count"] > min_tweets and
-                tweet["statuses"]["user"]["statuses_count"] < max_tweets and
-                tweet["statuses"]["user"]["friends_count"] < max_following and
-                tweet["statuses"]["user"]["lang"] == lang):
+        filtered_df = pd.DataFrame(columns=['ID', 'DateTime', 'Username', 
+                                            'tweet_text', 'retweet count', 
+                                            'Tweet Name', 'followers', 
+                                            'friends count', 'status count', 
+                                            'Search Coordinates'])
 
+        for i, row in df.iterrows():
+            if (row["followers"] > max_followers): # and
+                # row["status count"] < min_tweets and
+                # row["status count"] > max_tweets and
+                # row["friends count"] > max_following):
+
+                print("Found Tweet")
+                # filtered_df.append(row)
+                df.drop(df.index[[7]])
+                
+            else:
+                print(row["status count"])
+                
+                
 
         return df
